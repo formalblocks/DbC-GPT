8dd5971e33ecd9f20654f8008d331653dc7c70f3
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2024-05-30 11:02:15 -0300

    refactor: add tuple and dict import

--- solc_verify_generator/main.py
+++ solc_verify_generator/main.py
@@ -4,9 +4,11 @@ import os
 import re
 import string
 import argparse
+from typing import Tuple, Dict
 
-SOLC="solc"
-SPEC_PATH=os.path.join("temp","spec.sol_json.ast")
+SOLC = "solc"
+SPEC_PATH = os.path.join("temp", "spec.sol_json.ast")
+
 
 def call_solc(file_path):
     if os.path.isfile(SPEC_PATH):
@@ -15,6 +17,7 @@ def call_solc(file_path):
     command = [SOLC, file_path, "--ast-compact-json", "-o", "temp"]
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     return result
+
 
 def process_annotations(annotations, state_variables, prefix):
     for key, value in annotations.items():
@@ -26,17 +29,20 @@ def process_annotations(annotations, state_variables, prefix):
 
         annotations[key] = add_triple_bars(processed_annotation)
 
-def add_prefix(annotation: string, state_variables: dict, prefix):
+
+def add_prefix(annotation: str, state_variables: Dict[str, dict], prefix: str) -> str:
     for state_variable_name in state_variables.keys():
         annotation = annotation.replace(state_variable_name, prefix + "." + state_variable_name)
     return annotation
 
-def remove_old_ref(annotation, prefix):
+
+def remove_old_ref(annotation: str, prefix: str) -> str:
     pattern = r'__verifier_old_uint\s*\(\s*(nw.*?)\s*\)'
     replacement = lambda match: match.group(1).replace(prefix, f'{prefix}_old')
     return re.sub(pattern, replacement, annotation)
 
-def add_triple_bars(value):
+
+def add_triple_bars(value: str) -> str:
     lines = value.splitlines()
     new_annotation = ""
     for line in lines:
@@ -44,7 +50,7 @@ def add_triple_bars(value):
     return new_annotation
 
 
-def generate_merge(spec, imp_template, merge_file_path, prefix=None):
+def generate_merge(spec: str, imp_template: str, merge_file_path: str, prefix: str = None):
     result = call_solc(spec)
     if result.returncode:
         # Something has gone wrong compiling the solidity code
@@ -59,7 +65,8 @@ def generate_merge(spec, imp_template, merge_file_path, prefix=None):
     with open(merge_file_path, 'w') as merge_file:
         merge_file.write(merge_contract)
 
-def parse_ast() -> tuple[dict, dict]:
+
+def parse_ast() -> Tuple[Dict[str, dict], Dict[str, dict]]:
     # Fixing this for simplicity for the time being
     annotations, state_variables = dict({}), dict({})
     with open(SPEC_PATH, 'r') as spec_file:
@@ -67,8 +74,9 @@ def parse_ast() -> tuple[dict, dict]:
         for node in spec_dict["nodes"]:
             if node["nodeType"] == "ContractDefinition":
                 parse_contract(node, annotations, state_variables)
-        
+
     return annotations, state_variables
+
 
 def parse_contract(contract_json, annotations, state_variables):
     for node in contract_json["nodes"]:
@@ -77,15 +85,17 @@ def parse_contract(contract_json, annotations, state_variables):
         if node["nodeType"] == "VariableDeclaration":
             parse_state_variable(node, state_variables)
 
+
 def parse_state_variable(node_json, state_variables):
     name = node_json["name"]
     state_variables[name] = node_json
+
 
 def parse_function(function_json, annotations):
     annotation = function_json["documentation"]
     name = function_json["name"]
     annotations[name] = annotation
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("solc-verify contract generation")
@@ -95,6 +105,5 @@ if __name__ == "__main__":
     parser.add_argument("merge_template_file_path", help="The path to the merge template file.", type=str)
     parser.add_argument("merge_output_file_path", help="The path to the merge output file.", type=str)
     args = parser.parse_args()
-    
+
     generate_merge(args.spec_file_path, args.merge_template_file_path, args.merge_output_file_path, prefix=args.prefix)
-    # generate_merge("./ERC20/spec.sol", "./ERC20/imp/ERC20_merge.template", "./ERC20/imp/ERC20_merge.sol")
